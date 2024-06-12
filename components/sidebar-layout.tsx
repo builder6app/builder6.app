6e3121122bf13b7399dab603f3286f9f1f394a69
殷亮辉 <yinlianghui@hotoa.com>
2024-06-12 17:58:41 +0800

    sidbar layout组件在project 配置了菜单时应该有top边框

--- components/sidebar-layout.tsx
+++ components/sidebar-layout.tsx
@@ -2,7 +2,7 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2024-06-04 00:52:48
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2024-06-12 17:30:49
+ * @LastEditTime: 2024-06-12 17:57:20
  * @FilePath: /builder6/frontend/src/components/sidebar-layout.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -80,7 +80,9 @@ export function SidebarLayout({
     <Layout>
       <style jsx>{`
         .sidebar-layout{
-          ${topOffset ? "min-height: calc(100svh - " + topOffset + "px);" : ""}
+          ${topOffset ? 
+            "min-height: calc(100svh - " + topOffset + "px);\
+            border-top: solid 1px #ddd;" : ""};}
         }
       `}</style>
       <style jsx>{`
