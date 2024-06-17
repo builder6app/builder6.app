cd9a394e014387804f8b156e3d7d2e48af9b8295
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2024-06-17 15:26:10 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,21 +5,6 @@
  */
 import '@/styles/tailwind.css'
 import './globals.css';
-import { headers } from 'next/headers'
-import BuilderJS from '@builder6/builder6.js' 
-import { RenderBuilderContent } from '@/components/builder6';
-import { getSidebarItemsSection, getSidebarHomeSection } from './_lib/tabs';
-import { SidebarLayout } from '@/components/sidebar-layout'
-import { Navbar } from '@/components/navbar'
-import {
-  Sidebar,
-  SidebarBody,
-  SidebarHeader
-} from '@/components/sidebar'
-import Script from 'next/script';
-import { builder, Builder } from '@builder6/sdk';
-import { isEmpty } from 'lodash';
-
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
 
