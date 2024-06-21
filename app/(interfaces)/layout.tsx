31477f62076eb7c51c861fae662b624980394bc7
殷亮辉 <yinlianghui@hotoa.com>
2024-06-21 16:33:13 +0800

    [page菜单] 新窗口打开属性不生效

--- app/(interfaces)/layout.tsx
+++ app/(interfaces)/layout.tsx
@@ -38,7 +38,9 @@ const NAV_DEFAULT = `
                 <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                     {% for tab in tabs %}
                     <li>
-                        <a href="{{ tab.url }}" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">{{ tab.name }}</a>
+                        <a href="{{ tab.url }}" {% if tab.is_new_window %}target="_blank"{% endif %} class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                          {{ tab.name }}
+                        </a>
                     </li>
                     {% endfor %}
                 </ul>
