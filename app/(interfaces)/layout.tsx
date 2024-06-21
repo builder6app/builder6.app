275e5e28bba2161453bec6e3a33ba570dd7c3d7f
Jack <zhuangjianguo@gmail.com>
2024-06-22 01:57:07 +0800

    Update layout.tsx

--- app/(interfaces)/layout.tsx
+++ app/(interfaces)/layout.tsx
@@ -72,7 +72,7 @@ export default async function AppLayout( { params, children }: PageProps) {
   // 使用正则表达式提取前缀
   let projectId = project_id;
   // const baseId = "meta-builder6-com";
-  const baseId = `spc-${params.spaceId}`;
+  const baseId = `spc-${space}`;
 
   if (!projectId) return (<>projectId not found</>);
 
